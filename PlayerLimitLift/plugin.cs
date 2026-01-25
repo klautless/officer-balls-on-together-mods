05b53fd313f3e099932a266de991c80e6dc434e3
klautless <86319608+klautless@users.noreply.github.com>
2026-01-25 16:34:45 -0500

    Create plugin.cs

---
+++ PlayerLimitLift/plugin.cs
@@ -0,0 +1,33 @@
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlayerLimitLift.patches;
+
+namespace PlayerLimitLift
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string modGUID = "officerballs.PlayerLimitLift";
+        public const string modName = "Player Limit Lift";
+        public const string modVersion = "1.0.0.0";
+
+        private Harmony harmony = new Harmony(modGUID);
+
+        public ManualLogSource mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+
+
+        void Awake()
+        {
+            mls.LogInfo("Player Limit Lifted");
+            harmony.PatchAll(typeof(PlayerLimitPatch));
+        }
+
+    }
+}
