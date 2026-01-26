feb3b13e7bf3248302598385507e93f2315191e6
klautless <86319608+klautless@users.noreply.github.com>
2026-01-25 23:43:48 -0500

    Create chat_opacitypatch.cs

---
+++ ChatTweaks/patches/chat_opacitypatch.cs
@@ -0,0 +1,79 @@
+using BepInEx;
+using BepInEx.Logging;
+using DG.Tweening;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.EventSystems;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+using System.Drawing.Printing;
+
+namespace ChatTweaks.patches
+{
+    [HarmonyPatch(typeof(UIManager))]
+    internal class UIManagerPatch
+    {
+        //internal ManualLogSource mls;
+        [HarmonyPatch("Update")]
+        //[HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        public static void patchReset(ref float ____messageTimer)
+        {
+            ____messageTimer = 0f;
+        }
+        [HarmonyPatch("Awake")]
+        [HarmonyPostfix]
+        public static void outlineChanger( ref TextMeshProUGUI ____messageTextForFont)
+        {
+            Material mat = ____messageTextForFont.fontSharedMaterial;
+
+            mat.EnableKeyword("OUTLINE_ON");
+            mat.SetFloat("_OutlineWidth", 0.25f);
+            mat.SetColor("_OutlineColor", new Color32(20, 20, 20, 205));
+            ____messageTextForFont.UpdateMeshPadding();
+            
+        }
+        [HarmonyPatch("SetMessagePanelActiveness")]
+        [HarmonyPrefix]
+        public static bool ActivenessTweaker( ref TextMeshProUGUI ____messagePlaceholderText,
+        ref Color ____messagePlaceholderActiveColor,
+        ref float ____uiFadeDuration,
+        ref GameObject ____messagePanel,
+        ref List<Image> ____messagePanelImages,
+        ref List<TextMeshProUGUI> ____messagePanelTexts,
+        ref Image ____globalScrollImage, ref Image ____localScrollImage,
+        UIManager __instance)
+        {
+            
+            ____globalScrollImage.raycastTarget = true;
+            ____localScrollImage.raycastTarget = true;
+            __instance.IsMessagePanelActivated = true;
+            ____messagePlaceholderText.DOColor(____messagePlaceholderActiveColor, ____uiFadeDuration);
+            
+            ____messagePanel.gameObject.SetActive(value: true);
+            //StartCoroutine(SetMessageFontOutline(!isActive && MonoSingleton<OverlayManager>.I.ResolutionMode != ResolutionMode.Normal));
+            float a = 1f;
+            for (int i = 0; i < ____messagePanelImages.Count; i++)
+            {
+                Color color = ____messagePanelImages[i].color;
+                color.a = a;
+                ____messagePanelImages[i].DOColor(color, ____uiFadeDuration);
+            }
+            for (int j = 0; j < ____messagePanelTexts.Count; j++)
+            {
+                Color color2 = ____messagePanelTexts[j].color;
+                color2.a = a;
+                ____messagePanelTexts[j].DOColor(color2, ____uiFadeDuration);
+            }
+            return false;
+        }
+
+    }
+}
