a8ec195316d14ceba4a414089b2df435a4d39f97
klautless <86319608+klautless@users.noreply.github.com>
2026-01-26 16:59:49 -0500

    Create plugin.cs

---
+++ Teleport/plugin.cs
@@ -0,0 +1,27 @@
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Teleport.patches;
+
+namespace Teleport
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string modGUID = "officerballs.Teleport";
+        public const string modName = "Teleport";
+        public const string modVersion = "1.0.0.0";
+
+        private Harmony harmony = new Harmony(modGUID);
+
+        public ManualLogSource mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+        void Awake()
+        {
+            mls.LogInfo("Teleportation system loaded.");
+            
+            harmony.PatchAll(typeof(TeleportPatch));
+        }
+
+    }
+}
