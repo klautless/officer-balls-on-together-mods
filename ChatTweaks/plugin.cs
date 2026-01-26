9f3fb4a55fa6ceff5314a25318a3702faaad4699
klautless <86319608+klautless@users.noreply.github.com>
2026-01-25 23:40:01 -0500

    Add files via upload

---
+++ ChatTweaks/plugin.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChatTweaks.patches;
+
+namespace ChatTweaks
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string modGUID = "officerballs.chatTweaks";
+        public const string modName = "Chat Tweaks";
+        public const string modVersion = "1.0.0.0";
+
+        private Harmony harmony = new Harmony(modGUID);
+
+        public ManualLogSource mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+
+
+        void Awake()
+        {
+            mls.LogInfo("Chat tweaked");
+            harmony.PatchAll(typeof(UIManagerPatch));
+            harmony.PatchAll(typeof(TimeStampsPatch));
+        }
+
+    }
+}
