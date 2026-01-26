031d7a1a337695a957fb26a06744c02f268a8cd9
klautless <86319608+klautless@users.noreply.github.com>
2026-01-26 17:00:14 -0500

    Create teleportpatch.cs

---
+++ Teleport/patches/teleportpatch.cs
@@ -0,0 +1,112 @@
+
+using HarmonyLib;
+using TMPro;
+using UnityEngine;
+
+namespace Teleport.patches
+{
+    [HarmonyPatch(typeof(PlayerPanelController))]
+    public static class TeleportPatch
+    {
+        public static bool setup = false;
+        public static int index = 1;
+        
+        public static PlayerController target = null;
+
+        [HarmonyPatch("Update")]
+        [HarmonyPrefix]
+        public static bool TelePatch()
+        {
+            int tempdigit = 0;
+            var fieldRef = AccessTools.FieldRefAccess<UIManager, TMP_InputField>("_messageInputField");
+            var instance = MonoSingleton<UIManager>.I;
+            if (fieldRef(instance).isFocused) return true;
+            if(Input.GetKeyDown(KeyCode.E) && Input.GetKey(KeyCode.LeftShift))
+            {
+                if( NetworkSingleton<PlayerPanelController>.I.PlayerIDs.Count == 1 )
+                {
+                    //Debug.Log("can't teleport without other players!");
+                    return true;
+                }
+                index += 1;
+                tempdigit = 1;
+                goto changed;
+            }
+            else if(Input.GetKeyDown(KeyCode.Q) && Input.GetKey(KeyCode.LeftShift))
+            {
+                if( NetworkSingleton<PlayerPanelController>.I.PlayerIDs.Count == 1 )
+                {
+                    //Debug.Log("can't teleport without other players!");
+                    return true;
+                }
+                index -= 1;
+                tempdigit = -1;
+                goto changed;
+                
+            }
+            else if(Input.GetKeyDown(KeyCode.X) && Input.GetKey(KeyCode.LeftShift))
+            {
+                if( NetworkSingleton<PlayerPanelController>.I.PlayerIDs.Count == 1 )
+                {
+                    //Debug.Log("can't teleport without other players!");
+                    return true;
+                }
+                goto warp;
+            }
+            return true;
+
+            changed:
+                for (var i = 0; i < 2; i++)
+                {
+                    if (index < 0) index = NetworkSingleton<PlayerPanelController>.I.PlayerIDs.Count - 1;
+                    else if (index >= NetworkSingleton<PlayerPanelController>.I.PlayerIDs.Count ) index = 0;
+                    target = NetworkSingleton<PlayerPanelController>.I.PlayerTransforms[index].GetComponent<PlayerController>();
+                    var taname = target.PlayerNameText.text;
+                    var ourname = MonoSingleton<DataManager>.I.PlayerData.Name;
+                    if (taname != ourname ) break;
+                    if (tempdigit == 1) index+=1;
+                    else if (tempdigit == -1) index-=1;
+                
+                }
+                var namefieldRef = AccessTools.FieldRefAccess<UIManager, TextMeshProUGUI>("_playerText");
+                var nameinstance = MonoSingleton<UIManager>.I;
+                var playname = MonoSingleton<DataManager>.I.PlayerData.Name;
+
+                //int num = NetworkSingleton<PlayerPanelController>.I.PlayerIDs.IndexOf(info.sender);
+		        target = NetworkSingleton<PlayerPanelController>.I.PlayerTransforms[index].GetComponent<PlayerController>();
+                var tname = target.PlayerNameText.text;
+                namefieldRef(nameinstance).text = playname + "            -            " + tname + " selected";
+                //Debug.Log(tname + " selected.");
+                return false;
+            warp:
+                if (target)
+                {
+                    var controller = target.transform.GetComponentInChildren<PlayerMovementController>() as PlayerMovementController;
+                    NetworkSingleton<TextChannelManager>.I.MainPlayer.position = controller.transform.position;
+                    //Debug.Log("warped?");
+
+                    //Debug.Log(target.transform.GetComponentInChildren<PlayerMovementController>());
+                }
+                //else Debug.Log("no valid target!");
+
+                return false;
+        }
+
+        [HarmonyPatch("DespawnHandle")]
+        [HarmonyPostfix]
+        static public void DespawnHandlePatch( PlayerPanelController __instance)
+        {
+            if (index >= __instance.PlayerSteamIDs.Count) index = __instance.PlayerSteamIDs.Count - 1;
+            target = NetworkSingleton<PlayerPanelController>.I.PlayerTransforms[index].GetComponent<PlayerController>();
+            var namefieldRef = AccessTools.FieldRefAccess<UIManager, TextMeshProUGUI>("_playerText");
+            var nameinstance = MonoSingleton<UIManager>.I;
+            var playname = MonoSingleton<DataManager>.I.PlayerData.Name;
+
+            var tname = target.PlayerNameText.text;
+            namefieldRef(nameinstance).text = playname + "            -            " + tname + " selected";
+            //Debug.Log("player disconnected. " + tname + " selected.");
+                
+        }
+
+    }
+}
