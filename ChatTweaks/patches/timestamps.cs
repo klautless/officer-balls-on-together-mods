2ea3b731cb2be54173b0032d0cae8cf55b807559
klautless <86319608+klautless@users.noreply.github.com>
2026-01-25 23:43:26 -0500

    Create timestamps.cs

---
+++ ChatTweaks/patches/timestamps.cs
@@ -0,0 +1,50 @@
+using HarmonyLib;
+using System;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+using PurrNet;
+using System.Collections.Generic;
+
+namespace ChatTweaks.patches
+{
+    [HarmonyPatch(typeof(TextChannelManager))]
+    
+    public class TimeStampsPatch
+    {
+        [HarmonyPatch("AddMessageUI")]
+        [HarmonyPrefix]
+        public static bool MsgUIFix( string userName, string text, bool isLocal, int senderIndex, ref List<GameObject> ____messageObjectsLocal, ref List<GameObject> ____messageObjectsGlobal, ref TMP_Text ____textPrefab )
+        {
+            TMP_Text tMP_Text = UnityProxy.Instantiate( ____textPrefab, isLocal ? MonoSingleton<UIManager>.I.TextContentLocalTransform : MonoSingleton<UIManager>.I.TextContentGlobalTransform);
+            var stamp = DateTime.Now.ToString("h:mm tt");
+
+		    MonoSingleton<SFXManager>.I.PlayRodAppear();
+            tMP_Text.text = "[" + stamp + "] <color=#" + ScriptableSingleton<GameSettings>.I.MessageOthersColors[senderIndex] + "ff>" + userName + ":</color> " + text;
+            if (isLocal)
+            {
+                ____messageObjectsLocal.Add(tMP_Text.gameObject);
+                if (____messageObjectsLocal.Count > ScriptableSingleton<GameSettings>.I.LocalMessageLimitCount)
+                {
+                    GameObject obj = ____messageObjectsLocal[0];
+                    ____messageObjectsLocal.RemoveAt(0);
+                    UnityProxy.Destroy(obj);
+                }
+            }
+            else
+            {
+                ____messageObjectsGlobal.Add(tMP_Text.gameObject);
+                if (____messageObjectsGlobal.Count > ScriptableSingleton<GameSettings>.I.GlobalMessageLimitCount)
+                {
+                    GameObject obj2 = ____messageObjectsGlobal[0];
+                    ____messageObjectsGlobal.RemoveAt(0);
+                    UnityProxy.Destroy(obj2);
+                }
+            }
+            return false;
+
+        }
+
+    }
+}
