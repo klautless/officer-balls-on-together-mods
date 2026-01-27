d1f1b3fd318527a0067441d1c6ea6a4a9e2a9dd2
klautless <86319608+klautless@users.noreply.github.com>
2026-01-27 05:57:48 -0500

    Update playerjoined_patch.cs
    checks cfg; removed unnecessary using statements

--- ChatTweaks/patches/playerjoined_patch.cs
+++ ChatTweaks/patches/playerjoined_patch.cs
@@ -1,19 +1,5 @@
-using BepInEx;
-using BepInEx.Logging;
-using DG.Tweening;
+
 using HarmonyLib;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TMPro;
-using UnityEngine;
-using UnityEngine.PlayerLoop;
-using UnityEngine.EventSystems;
-using UnityEngine.Rendering;
-using UnityEngine.UI;
-using System.Drawing.Printing;
 
 namespace ChatTweaks.patches
 {
@@ -24,7 +10,10 @@ namespace ChatTweaks.patches
         [HarmonyPrefix]
         public static void joinSoundPatcher()
         {
-            NetworkSingleton<TextChannelManager>.I.MainSFXController.PlayPetSound(PetType.FisherBirdy);
+            if (Plugin.configJoinLeaveNoises.Value)
+            {
+                NetworkSingleton<TextChannelManager>.I.MainSFXController.PlayPetSound(PetType.FisherBirdy);
+            }
         }
 
     }
