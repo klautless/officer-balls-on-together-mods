aacc257270d78ee209eeb352a4a9173104ad712a
klautless <86319608+klautless@users.noreply.github.com>
2026-01-27 04:28:24 -0500

    Update plugin.cs
    versioning change v1.0.2

--- Teleport/plugin.cs
+++ Teleport/plugin.cs
@@ -10,7 +10,7 @@ namespace Teleport
     {
         public const string modGUID = "officerballs.Teleport";
         public const string modName = "Teleport";
-        public const string modVersion = "1.0.1.0";
+        public const string modVersion = "1.0.2.0";
 
         private Harmony harmony = new Harmony(modGUID);
 
