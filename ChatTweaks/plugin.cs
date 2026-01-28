f93f7c4108e4024593e2bb76f9f495c18baf85ae
klautless <86319608+klautless@users.noreply.github.com>
2026-01-28 14:08:17 -0500

    Update plugin.cs
    added configurable text size wrapper

--- ChatTweaks/plugin.cs
+++ ChatTweaks/plugin.cs
@@ -9,11 +9,12 @@ namespace ChatTweaks
     [BepInPlugin(modGUID, modName, modVersion)]
     public class Plugin : BaseUnityPlugin
     {
+        public static ConfigEntry<int> configTextSize  { get; private set; }
         public static ConfigEntry<bool> configMsgNoises  { get; private set; }
         public static ConfigEntry<bool> configJoinLeaveNoises  { get; private set; }
         public const string modGUID = "officerballs.chatTweaks";
         public const string modName = "Chat Tweaks";
-        public const string modVersion = "1.0.3.0";
+        public const string modVersion = "1.0.4.0";
 
         private Harmony harmony = new Harmony(modGUID);
 
@@ -25,10 +26,11 @@ namespace ChatTweaks
         {
             harmony.PatchAll(typeof(UIManagerPatch));
             harmony.PatchAll(typeof(TimeStampsPatch));
-            harmony.PatchAll(typeof(PlayerLeftPatch));
+            harmony.PatchAll(typeof(PlayerPanelPatch));
             harmony.PatchAll(typeof(joinSoundPatch));
             configMsgNoises = Config.Bind("General.Toggles", "PlayMessageNoises",true,"Enable or disable on-message noises (takes effect after restart)");
             configJoinLeaveNoises = Config.Bind("General.Toggles", "PlayJoinLeaveNoises",true,"Enable or disable on-leave/join noises (takes effect after restart)");
+            configTextSize = Config.Bind("General", "TextSize",48,"Wraps chat with a size tag (takes effect after restart)");
             mls.LogInfo("Chat tweaked");
         }
 
