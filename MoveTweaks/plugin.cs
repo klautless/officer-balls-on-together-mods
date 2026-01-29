6d93e3360e0064fc416d39625be9b0fce2c3db80
klautless <86319608+klautless@users.noreply.github.com>
2026-01-29 13:08:54 -0500

    Add files via upload

---
+++ MoveTweaks/plugin.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using MoveTweaks.patches;
+
+namespace MoveTweaks
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string modGUID = "officerballs.MoveTweaks";
+        public const string modName = "MoveTweaks";
+        public const string modVersion = "1.0.0.0";
+
+        private Harmony harmony = new Harmony(modGUID);
+
+        public ManualLogSource mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+        void Awake()
+        {
+            mls.LogInfo("Movement tweaks loaded.");
+            
+            harmony.PatchAll(typeof(SprintX));
+            harmony.PatchAll(typeof(SprintY));
+            harmony.PatchAll(typeof(MovementPatch));
+            harmony.PatchAll(typeof(BubblePatch));
+            harmony.PatchAll(typeof(InfiniteBubbles));
+        }
+
+    }
+}
