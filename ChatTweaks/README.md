ef2c2a2a5471e034a17a4b04829208edf97a4370
klautless <86319608+klautless@users.noreply.github.com>
2026-01-31 10:23:30 -0500

    Add files via upload

--- ChatTweaks/README.md
+++ ChatTweaks/README.md
@@ -1,4 +1,4 @@
-ChatTweaks:  
+# ChatTweaks:  
 - adds a timestamp to chat
 - adds outline to the font
 - prevents the chat window from fading
@@ -6,8 +6,20 @@ ChatTweaks:
 - players now emit a "has left the server" message
 - players now emit sounds when joining and leaving the server (fishing bird on join, frog on leaving)
   
-you can edit the config generated in BepInEx/config to disable the noises if desired.  
+# Commands  
   
-Manual Installation:  
-1. Install BepInEx as directed on GitHub.  
+- /help chat, /help chattweaks - brings up a list of commands in game.  
+- /textsize x - lets you wrap the entire chat with a size. game default: 48.  
+- /textcolor 123456 - lets you wrap the entire chat with a color. default: ffffff.  
+- /systemcolor 123456 - lets you wrap join/leave messages with a color. default: d5d6db.  
+- /outlinecolor 123456 - the color for font outline. default: 141414.  
+- /outlinewidth x.xx - outline width. default: 0.25  
+- /outlineopacity x - outline opacity ranging 0-255. default: 205.  
+- /usetimestamps - enable/disable the timestamping system. default: on.  
+- /mutetext - enable/disable the chat message noises. default: on.  
+- /mutejoinleave - enable/disable the join/leave noises. default: on.  
+
+# Manual Installation:  
+1. Install BepInEx as directed on GitHub/Thunderstore.  
 2. Extract archive to your game's installation, making sure to overwrite/merge the folders as prompted.
+
--- ChatTweaks/plugin.cs
+++ ChatTweaks/plugin.cs
@@ -1,4 +1,4 @@
-using BepInEx;
+﻿using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
@@ -10,11 +10,18 @@ namespace ChatTweaks
     public class Plugin : BaseUnityPlugin
     {
         public static ConfigEntry<int> configTextSize  { get; private set; }
+        public static ConfigEntry<string> configColorWrap { get; private set; }
+        public static ConfigEntry<string> configSystemColorWrap { get; private set; }
         public static ConfigEntry<bool> configMsgNoises  { get; private set; }
         public static ConfigEntry<bool> configJoinLeaveNoises  { get; private set; }
+        public static ConfigEntry<bool> configUseTimeStamps { get; private set; }
+        public static ConfigEntry<string> configOutlineColor { get; private set; }
+        public static ConfigEntry<float> configOutlineWidth { get; private set; }
+        public static ConfigEntry<int> configOutlineOpacity { get; private set; }
+
         public const string modGUID = "officerballs.chatTweaks";
         public const string modName = "Chat Tweaks";
-        public const string modVersion = "1.0.5.0";
+        public const string modVersion = "1.0.6.0";
 
         private Harmony harmony = new Harmony(modGUID);
 
@@ -25,15 +32,24 @@ namespace ChatTweaks
         void Awake()
         {
             harmony.PatchAll(typeof(UIManagerPatch));
-            harmony.PatchAll(typeof(TimeStampsPatch));
+            harmony.PatchAll(typeof(TextPatcher));
             harmony.PatchAll(typeof(PlayerPanelPatch));
             harmony.PatchAll(typeof(joinSoundPatch));
-            configMsgNoises = Config.Bind("General.Toggles", "PlayMessageNoises",true,"Enable or disable on-message noises (takes effect after restart)");
-            configJoinLeaveNoises = Config.Bind("General.Toggles", "PlayJoinLeaveNoises",true,"Enable or disable on-leave/join noises (takes effect after restart)");
-            configTextSize = Config.Bind("General", "TextSize",48,"Wraps chat with a size tag (takes effect after restart)");
+
+            configMsgNoises = Config.Bind("General.Toggles", "PlayMessageNoises", true, "Enable or disable on-message noises.");
+            configJoinLeaveNoises = Config.Bind("General.Toggles", "PlayJoinLeaveNoises", true, "Enable or disable on-leave/join noises.");
+            configTextSize = Config.Bind("General", "TextSize", 48, "Wraps chat with a size tag.");
+            configColorWrap = Config.Bind("General", "TextColor", "ffffff", "Wraps chat with a color tag.");
+            configSystemColorWrap = Config.Bind("General", "SystemTextColor", "d5d6db", "Wraps system messages with a color tag.");
+
+            configOutlineColor = Config.Bind("General", "OutlineColor", "141414", "Outline's color.");
+            configOutlineWidth = Config.Bind("General", "OutlineWidth", 0.25f, "Outline's width.");
+            configOutlineOpacity = Config.Bind("General", "OutlineOpacity", 205, "Outline's opacity.");
+
+            configUseTimeStamps = Config.Bind("General","UseTimestamps", true, "Whether to use timestamps in chat.");
+
             mls.LogInfo("Chat tweaked");
         }
 
     }
 }
-
