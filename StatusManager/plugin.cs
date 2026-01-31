4f016095fde1d594a93893bdbdd3be930a9b9b8c
klautless <86319608+klautless@users.noreply.github.com>
2026-01-31 10:44:51 -0500

    Update plugin.cs
    1.0.2 changed /help to /help status (will direct player of /help options)

--- StatusManager/plugin.cs
+++ StatusManager/plugin.cs
@@ -27,7 +27,7 @@ namespace StatusMessage
 
         public const string modGUID = "officerballs.StatusManager";
         public const string modName = "Status Manager";
-        public const string modVersion = "1.0.1.0";
+        public const string modVersion = "1.0.2.0";
 
         private Harmony harmony = new Harmony(modGUID);
 
@@ -69,3 +69,4 @@ namespace StatusMessage
 
     }
 }
+
