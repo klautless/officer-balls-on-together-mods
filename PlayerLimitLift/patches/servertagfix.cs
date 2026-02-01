276cf2e6ce699d65bf879d84ddf09c6f9184c18b
klautless <86319608+klautless@users.noreply.github.com>
2026-02-01 10:50:46 -0500

    Update servertagfix.cs
    tightened formatting; now always applies modded tag

--- PlayerLimitLift/patches/servertagfix.cs
+++ PlayerLimitLift/patches/servertagfix.cs
@@ -5,24 +5,15 @@ using System.Collections.Generic;
 namespace PlayerLimitLift.patches
 {
     [HarmonyPatch(typeof(MultiplayerManager))]
-    
     public class AddModdedTag
     {
         [HarmonyPatch("CreateLobby")]
         [HarmonyPrefix]
         public static void NameChanger( ref List<bool> ___FilterSocialTags )
         {
-            //Debug.Log("branch reached");
             var val = MonoSingleton<MultiplayerManager>.I.FilterPlayerValue;
             var text = MonoSingleton<MainMenuUIController>.I.CreateSessionNameInputField.text;
-            if (val > 16)
-            {
-                ___FilterSocialTags[4] = true;
-
-                //MonoSingleton<MainMenuUIController>.I.CreateSessionNameInputField.text = "[MODDED] " + text;
-            }
-
+            ___FilterSocialTags[4] = true;
         }
-
     }
 }
