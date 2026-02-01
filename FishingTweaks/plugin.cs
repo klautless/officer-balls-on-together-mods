d8662397366e4c3a487c52c64fb4bb8c52a086f3
klautless <86319608+klautless@users.noreply.github.com>
2026-02-01 12:13:24 -0500

    Update plugin.cs
    maintenance patch, when you host it now applies modded tag

--- FishingTweaks/plugin.cs
+++ FishingTweaks/plugin.cs
@@ -10,18 +10,16 @@ namespace FishingTweaks
     {
         public const string modGUID = "officerballs.FishingTweaks";
         public const string modName = "Fishing Tweaks";
-        public const string modVersion = "1.0.1.0";
+        public const string modVersion = "1.0.2.0";
 
         private Harmony harmony = new Harmony(modGUID);
 
         public ManualLogSource mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
 
-
-
         void Awake()
         {
             mls.LogInfo("Fishing tweaked.");
-            
+            harmony.PatchAll(typeof(AddModdedTag));
             harmony.PatchAll(typeof(FishingPatch));
             harmony.PatchAll(typeof(FishingPatch2));
             harmony.PatchAll(typeof(GroundCheckPatch));
