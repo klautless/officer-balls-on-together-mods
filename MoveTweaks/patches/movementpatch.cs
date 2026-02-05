1a92a83bc7599076cb2278f44b41d1f1d39dd1a7
klautless <86319608+klautless@users.noreply.github.com>
2026-02-05 07:31:56 -0500

    Update movementpatch.cs
    minor patch changing the 'if' tree in the movement patch to raycast less

--- MoveTweaks/patches/movementpatch.cs
+++ MoveTweaks/patches/movementpatch.cs
@@ -12,17 +12,20 @@ namespace MoveTweaks.patches
         [HarmonyPrefix]
         public static void MovePatch( PlayerMovementController __instance)
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit, 1000f) && Input.GetMouseButton(2))
+            if (Input.GetMouseButton(2))
             {
-                var playerpos = NetworkSingleton<TextChannelManager>.I.MainPlayer.transform;// = controller.transform.position;
-                Vector3 worldPosition = hit.point;
-                worldPosition.y = playerpos.position.y;
-                playerpos.LookAt(worldPosition);
-                __instance.transform.rotation = Quaternion.Slerp(__instance.transform.rotation, playerpos.rotation, 5f * Time.deltaTime);;
-                
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, 1000f))
+                {
+                    var playerpos = NetworkSingleton<TextChannelManager>.I.MainPlayer.transform;// = controller.transform.position;
+                    Vector3 worldPosition = hit.point;
+                    worldPosition.y = playerpos.position.y;
+                    playerpos.LookAt(worldPosition);
+                    __instance.transform.rotation = Quaternion.Slerp(__instance.transform.rotation, playerpos.rotation, 5f * Time.deltaTime);;
+                    
+                }
             }
         }
 
