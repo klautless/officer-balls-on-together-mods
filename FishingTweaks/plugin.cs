b9080af2c5ba850b0807059c1c35f638cc685209
klautless <86319608+klautless@users.noreply.github.com>
2026-02-07 10:04:41 -0500

    Update plugin.cs
    minor revision to 1.1.0, fixed mutes and added a few more cmds

--- FishingTweaks/plugin.cs
+++ FishingTweaks/plugin.cs
@@ -14,6 +14,8 @@ namespace FishingTweaks
         public static ConfigEntry<bool> configAutoRecast { get; private set; }
         public static ConfigEntry<bool> configInfiniteBait { get; private set; }
         public static ConfigEntry<bool> configMuteFishing  { get; private set; }
+        public static ConfigEntry<bool> configMuteMinigame { get; private set; }
+        public static ConfigEntry<bool> configMuteFishingDuringFocus { get; private set; }
         public const string modGUID = "officerballs.FishingTweaks";
         public const string modName = "Fishing Tweaks";
         public const string modVersion = "1.1.0.0";
@@ -33,12 +35,15 @@ namespace FishingTweaks
             harmony.PatchAll(typeof(ClickSender));
             harmony.PatchAll(typeof(AddCommands));
             harmony.PatchAll(typeof(BlockSounds));
+            harmony.PatchAll(typeof(BlockSounds2));
 
             configHideAnnoyingCatches = Config.Bind("General.Toggles", "HideAnnoyingCatches", false, "Resizes oversized fish from other players.");
-            configMuteFishing = Config.Bind("General.Toggles", "MuteFishing", false, "Gets rid of the annoying bobber noise.");
             configAutoCatch = Config.Bind("General.Toggles", "AutoCatch", false, "Enable/disable automatic catching.");
             configAutoRecast = Config.Bind("General.Toggles", "AutoRecast", false, "Automatically recast to last position when fishing.");
             configInfiniteBait = Config.Bind("General.Toggles", "InfiniteBait", false, "Infinite bait.");
+            configMuteMinigame = Config.Bind("General.Sound", "MuteMinigame", false, "Mutes SFX from the fishing minigame.");
+            configMuteFishing = Config.Bind("General.Sound", "MuteFishing", false, "Gets rid of the annoying bobber noise.");
+            configMuteFishingDuringFocus = Config.Bind("General.Sound", "MuteDuringFocus", false, "Mute all fishing during Pomodoro focus.");
         }
 
     }
